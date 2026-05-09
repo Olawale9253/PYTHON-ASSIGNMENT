74ee547a7b3e33271e04ed5413bd71cc0f68b4de
Olawale <taofeekolawale159@gmail.com>
2026-05-09 17:56:16 +0100

    Task uodates

---
+++ lettergrade.py
@@ -0,0 +1,16 @@
+grade = int(input("Enter your mark: "))
+
+if grade >= 90:
+    print("Grade: A")
+else:
+    if grade >= 80:
+        print("Grade: B")  
+    else:
+        if grade >= 70:
+            rint("Grade: C")
+        else:
+            if grade >= 60:
+                print("Grade: D")
+            else:
+                print("Grade: F")
+
